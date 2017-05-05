6894219af8fed2291ae0285e4e4f28433df6ac2e
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-05-05 14:09:52 +0200

    Plot min, max and average agents count

--- agents_count.py
+++ agents_count.py
@@ -1,6 +1,8 @@
 import sys
+import os
 import matplotlib.pyplot as plt
 
+from operator import add
 from pyspark import SparkContext
 from spark_utils import average_by_key
 from results_loader import load_results
@@ -9,8 +11,8 @@ from results_loader import load_results
 APP_NAME = 'EMAS - Agents count'
 
 
-def aggregate(rdd):
-    rdd = average_by_key(rdd)
+def aggregate(rdd, op=None):
+    rdd = rdd.reduceByKey(op) if op else average_by_key(rdd)
     rdd = rdd.map(lambda ((_experiment, second), value): (second, value))
     rdd = average_by_key(rdd)
     rdd = rdd.map(lambda (second, value): (second, int(value)))
@@ -19,24 +21,68 @@ def aggregate(rdd):
     return zip(*data)
 
 
-def plot(data_points):
-    x, y = data_points
-    plt.plot(x, y)
-    plt.title('Average agents count per population')
+def plot(data_sets):
+    for series, data_points in data_sets.iteritems():
+        x, y = data_points
+        plt.plot(x, y, label=series)
+
     plt.xlabel('Time [s]')
     plt.ylabel('Agents count')
-    plt.axis([5, 80, 0, 150])
+    # plt.xlim(5, 85)
     plt.grid(True)
-    plt.show()
-
+    plt.legend(loc='lower right')
 
 if __name__ == '__main__':
     results_dir = sys.argv[1]
 
     sc = SparkContext("local", APP_NAME)
 
-    rdd = load_results(sc, results_dir, 'agents_count')
+    plt.figure(1)
 
-    data_points = aggregate(rdd)
+    plt.subplot(221)
+    plt.title('Min agents count')
+    plt.ylim(-10, 150)
 
-    plot(data_points)
+    data_sets = {}
+    for series in os.listdir(results_dir):
+        series_dir = os.path.join(results_dir, series)
+        rdd = load_results(sc, series_dir, 'agents_count')
+        data_points = aggregate(rdd, min)
+        data_sets[series] = data_points
+    plot(data_sets)
+
+    plt.subplot(222)
+    plt.title('Max agents count')
+
+    data_sets = {}
+    for series in os.listdir(results_dir):
+        series_dir = os.path.join(results_dir, series)
+        rdd = load_results(sc, series_dir, 'agents_count')
+        data_points = aggregate(rdd, max)
+        data_sets[series] = data_points
+    plot(data_sets)
+
+    plt.subplot(223)
+    plt.title('Average agents count')
+    plt.ylim(-10, 200)
+
+    data_sets = {}
+    for series in os.listdir(results_dir):
+        series_dir = os.path.join(results_dir, series)
+        rdd = load_results(sc, series_dir, 'agents_count')
+        data_points = aggregate(rdd)
+        data_sets[series] = data_points
+    plot(data_sets)
+
+    plt.subplot(224)
+    plt.title('Total agents count')
+
+    data_sets = {}
+    for series in os.listdir(results_dir):
+        series_dir = os.path.join(results_dir, series)
+        rdd = load_results(sc, series_dir, 'agents_count')
+        data_points = aggregate(rdd, add)
+        data_sets[series] = data_points
+    plot(data_sets)
+
+    plt.show()
